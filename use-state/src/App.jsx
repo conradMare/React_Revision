ab07a08cbca5a9442c2b3b39209f801d6fcf5fd1
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-29 10:12:28 +0200

    2024/07/29(am)
    React useSate() hook - Counter program

--- use-state/src/App.jsx
+++ use-state/src/App.jsx
@@ -6,11 +6,13 @@
 //              AND a setter function to update its value in the Virtual DOM.
 //              [name, setName]
 
-import MyComponent from "./MyComponent";
+// import MyComponent from "./MyComponent";
+import Counter from "./Counter";
 
 function App() {
   return (
-    <MyComponent />
+    // <MyComponent />
+    <Counter />
   );
 }
 
