72b0160c542af4a6b6dad83a4758368536d0b44e
conradMare <158014474+conradMare@users.noreply.github.com>
2024-07-31 06:32:11 +0200

    2024/07/31(am)
    React Update OBJECT in state() - Completed

---
+++ update-objects-in-state/src/MyComponent.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function MyComponent() {
+
+    const [car, setCar] = useState({ year: 2024, make: "Audi", model: "A1" });
+
+    function handleYearChange(e) {
+        setCar(c => ({ ...c, year: e.target.value }));
+    }
+
+    function handleMakeChange(e) {
+        setCar(c => ({ ...c, make: e.target.value }));
+    }
+
+    function handleModelChange(e) {
+        setCar(c => ({ ...c, model: e.target.value }));
+    }
+
+    return (
+        <div>
+            <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
+
+            <input type="number" value={car.year} onChange={handleYearChange} />
+            <br />
+            <input type="text" value={car.make} onChange={handleMakeChange} />
+            <br />
+            <input type="text" value={car.model} onChange={handleModelChange} />
+        </div>
+    );
+}
+
+export default MyComponent
