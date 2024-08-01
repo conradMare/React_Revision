488d5a1b3c4b58ae317fd4d22ba6adf4c2ac65f0
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-01 07:33:35 +0200

    2024/08/01(am)
    React Digital Clock App - CSS added

--- digital-clock-app/src/DigitalClock.jsx
+++ digital-clock-app/src/DigitalClock.jsx
@@ -2,9 +2,11 @@ import { useState, useEffect } from "react";
 
 function DigitalClock() {
     return(
-        <>
-
-        </>
+        <div className="clock-container">
+            <div className="clock">
+                <span>00:00:00</span>
+            </div>
+        </div>
     );
 }
 
