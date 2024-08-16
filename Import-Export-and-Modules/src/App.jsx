f62fdcbc9d1108e661a4adebaf74dcf075f8608d
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-16 11:01:55 +0200

    2024/08/16(am)
    JavaScript ES6 - Import, Export and Modules

---
+++ Import-Export-and-Modules/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Heading from "./components/Heading";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+        <div>
+            <Heading />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ Import-Export-and-Modules/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+
+    const date = new Date();
+    const year = date.getFullYear();
+
+    return(
+        <div className="footer">
+            <p>Copyright {year}</p>
+        </div>
+    );
+}
+
+export default Footer;
