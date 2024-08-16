f112676fb16523e9c367d5504dacd7911fbe5f43
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-16 09:57:55 +0200

    2024/08/16(am)
    React Styling Practice - Complete

--- React-Styling-Practice/src/main.jsx
+++ React-Styling-Practice/src/main.jsx
@@ -2,9 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const date = new Date();
+const currentTime = date.getHours();
+
+let greeting;
+
+const customStyle = {
+  color: ""
+}
+
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Evening";
+  customStyle.color = "blue";
+}
+
 ReactDOM.render(
-  <div>
-    <h1>Hello World!</h1>
+  <div className="heading">
+    <h1 style={customStyle}>{greeting}</h1>
   </div>,
   document.getElementById('root')
 );
