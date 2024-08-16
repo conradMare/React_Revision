cb49be53d865611a7eb03bdf4c478de97d556166
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-16 09:27:15 +0200

    2024/08/16(am)
    Inline Styling for React Elements - Complete

--- Inline-Styling-for-React-Elements/src/main.jsx
+++ Inline-Styling-for-React-Elements/src/main.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
+
+const customStyle = {
+  color: "red",
+  fontSize: "50px",
+  background: "green",
+  border: "1px solid black",
+  boxShadow: "5px 5px gray",
+  borderRadius: "20px"
+}
+
+customStyle.color = "blue";
 
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
+    <h1 style={customStyle}>Hello World!</h1>
+    <button className="button">Click Me!</button>
   </div>,
   document.getElementById('root')
 );
