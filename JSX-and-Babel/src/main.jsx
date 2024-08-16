70226f5ec6b745a33841bad6fa48613560c725ad
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-16 07:12:22 +0200

    2024/08/16(am)
    JSX and Babel - Creating two elements with a div

--- JSX-and-Babel/src/main.jsx
+++ JSX-and-Babel/src/main.jsx
@@ -1,4 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>Have to use a div to create two html elements</p>
+  </div>,
+  document.getElementById('root'));
