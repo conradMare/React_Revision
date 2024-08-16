4efa68c4d8a17ba4cc9b4c50141dfcda388b5b6f
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-16 11:42:20 +0200

    2024/08/16(am)
    JavaScript ES6 Import, Export and Modules Practice - complete

--- Import-Export-and-Modules-Practice/src/main.jsx
+++ Import-Export-and-Modules-Practice/src/main.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { add, subtract, multiply, divide } from "./calculator";
 
 ReactDOM.render(
-  <App />,
+  <ul>
+    <li>Add: 5 + 3 = {add(5, 3)}</li>
+    <li>Subtract: 5 - 3 = {subtract(5, 3)}</li>
+    <li>Multiply: 5 * 3 = {multiply(5, 3)}</li>
+    <li>Divide: 5 / 3 = {divide(5, 3)}</li>
+  </ul>,
   document.getElementById('root')
 );
