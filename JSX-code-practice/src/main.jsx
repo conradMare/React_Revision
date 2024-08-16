82017a2f6911c118fc289cffeee377e301c01a2b
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-16 07:17:13 +0200

    2024/08/16(am)
    JSX Code Practice

---
+++ JSX-code-practice/src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <ol>
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ol>
+  </div>,
+  document.getElementById('root')
+);
