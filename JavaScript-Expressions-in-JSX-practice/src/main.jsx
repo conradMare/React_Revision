a45cf40350f8fc27b08a7ba5f2bb29eed26bada9
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-16 08:17:02 +0200

    2024/08/16(am)
    JavaScript Expressions in JSX Practice - Complete

--- JavaScript-Expressions-in-JSX-practice/src/main.jsx
+++ JavaScript-Expressions-in-JSX-practice/src/main.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const fName = "Conrad";
+const lName = "Mare";
+const year = new Date();
+
 ReactDOM.render(
   <div>
-    <h1>Hello World!</h1>
+    <p>Created by {`${fName} ${lName}`}</p>
+    <p>Copyright © {year.getFullYear()}</p>
   </div>,
   document.getElementById('root')
 );
