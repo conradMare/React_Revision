993b78eab8f60a76cb616c2cb18bc8da28a1738e
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-16 11:19:38 +0200

    2024/08/16(am)
    JavaScript ES6 - Import, Export and Modules - complete

--- Import-Export-and-Modules/src/main.jsx
+++ Import-Export-and-Modules/src/main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import "./index.css";
+import pi, {doublePi, triplePi} from "./math";
+
+// Wildcard imports are not encouraged in style guides: (Not specific on what you are importing)
+// import * as pi from "./math";
 
 ReactDOM.render(
   <div>
-    <App />
+    <ul>
+      <li>Pi: {pi}</li>
+      <li>Double Pi: {doublePi()}</li>
+      <li>Triple Pi: {triplePi()}</li>
+    </ul>
   </div>,
   document.getElementById('root')
 );
