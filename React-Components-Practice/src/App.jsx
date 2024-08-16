16904080bab94d57dad84f3fc1cc1bd11929293c
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-16 10:46:38 +0200

    2024/08/16(am)
    React Components Practice - Complete

--- React-Components-Practice/src/App.jsx
+++ React-Components-Practice/src/App.jsx
@@ -1,30 +1,11 @@
 import React from "react";
 import "./App.css";
-
-const date = new Date();
-const currentTime = date.getHours();
-
-let greeting;
-
-const customColor = {
-    color: ""
-}
-
-if(currentTime < 12) {
-    greeting = "Good Morning";
-    customColor.color = "red";
-} else if (currentTime < 18) {
-    greeting = "Good Afternoon";
-    customColor.color = "green";
-} else {
-    greeting = "Good Evening";
-    customColor.color = "blue";
-}
+import Heading from "./components/Heading";
 
 function App() {
     return (
-        <div className="heading">
-            <h1 style={customColor}>{greeting}</h1>
+        <div>
+            <Heading />
         </div>
     );
 }
