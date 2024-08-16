327751e076d506ab120c5e6233dfdb07002f2af1
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-16 08:04:56 +0200

    2024/08/16(am)
    JavaScript Expressions in JSX & ES6 Template Literals - Complete

--- JavaScript-Expressions-in-JSX-and-Template-Literals/src/main.jsx
+++ JavaScript-Expressions-in-JSX-and-Template-Literals/src/main.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name = "Conrad";
+const fName = "John";
+const lName = "Doe";
+const luckyNumber = 7;
+
 ReactDOM.render(
   <div>
-    <h1>Hello World</h1>
+    <h1>Hello {name}</h1>
+    <h2>Name with Template literals: {`${fName} ${lName}`}</h2>
+    <p>Your lucky number created with an expression is: {Math.floor(Math.random() * 10)}</p>
+    <p>Your luck number that is hard-coded is: {luckyNumber}</p>
   </div>,
   document.getElementById('root')
 );
