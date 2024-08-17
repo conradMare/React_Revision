865af754afb0a9560dd122e9d9997ed7278584c5
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-17 07:18:33 +0200

    2024/08/17(am)
    React Props Practice - Project Setup

---
+++ React-Props-Practice/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+
+    const year = new Date().getFullYear();
+
+    return (
+        <div className="footer">
+            <p>Copyright {year}</p>
+        </div>
+    );
+}
+
+export default Footer;
