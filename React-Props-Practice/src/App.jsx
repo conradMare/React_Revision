c85c0cf634408addf3df7de1075f38f644ab4795
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-17 08:05:03 +0200

    2024/08/17(am)
    React Props Practice - Complete

--- React-Props-Practice/src/App.jsx
+++ React-Props-Practice/src/App.jsx
@@ -1,14 +1,31 @@
 import React from "react";
+import Card from "./component/Card";
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 function App() {
     return (
         <div>
-            <Header />
-            <Footer />
+            <h1 className="heading">Images</h1>
+            <Card
+                name="Beyonce"
+                img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+                phone="+123 456 789"
+                email="b@beyonce.com"
+            />
+            <Card 
+                name="Jack Bauer"
+                img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+                phone="+987 654 321"
+                email="jack@nowhere.com"
+            />
+            <Card 
+                name="Chuck Norris"
+                img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+                phone="+918 372 574"
+                email="gmail@chucknorris.com"
+            />
         </div>
+
     );
 }
 
--- React-Props-Practice/src/main.jsx
+++ React-Props-Practice/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 
 ReactDOM.render(
   <div>
-  <App />
+    <App />
   </div>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
