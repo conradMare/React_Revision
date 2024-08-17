08a28dfbcb9780d95ecfc4f11c1afa5dfd161d95
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-17 09:16:55 +0200

    2024/08/17(am)
    React Dev Tools - Project Setup (React Props Repetition Complete)

---
+++ React-DevTools/src/Contacts.js
@@ -0,0 +1,22 @@
+const contacts = [
+    {
+        name: "Inspiration 1",
+        imgURL:
+            "https://cdn.shopify.com/s/files/1/0070/7032/files/churchhill-quote.png?v=1706739710",
+        description: "Success is not final, Failure is not fatal, It's the courage to continue that counts."
+    },
+    {
+        name: "Inspiration 2",
+        imgURL:
+            "https://cdn-fkmoj.nitrocdn.com/xvpOGZRTxJUhXKufpOYIruQcRqtvAAQX/assets/images/optimized/rev-4e1f421/media.briantracy.com/blog/wp-content/uploads/2024/01/23111415/Quote-2.png",
+        description: "Keep your face always toward the sunshine, and shadows will fall behind you."
+    },
+    {
+        name: "Inspiration 3",
+        imgURL:
+            "https://quotefancy.com/media/wallpaper/3840x2160/17003-Neale-Donald-Walsch-Quote-Life-begins-at-the-end-of-your-comfort.jpg",
+        description: "Life begins at the end of your comfort zone."
+    }
+];
+
+export default contacts;
