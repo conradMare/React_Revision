394b149a99a36c8e349da9428765c938f56035b2
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-19 10:23:15 +0200

    2024/08/19(am)
    React Conditional Rendering with the Ternary Operator & AND Operator - Project Setup

---
+++ React-Conditional-Rendering/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+      <form className="form">
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
