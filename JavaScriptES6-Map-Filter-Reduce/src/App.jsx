826408c1b110e3935ca9e8ec6bd6c3faefa53e69
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-19 08:08:55 +0200

    2024/-8/19(am)
    JavaScript ES6 Map/ Filter/ Reduce - Complete

--- JavaScriptES6-Map-Filter-Reduce/src/App.jsx
+++ JavaScriptES6-Map-Filter-Reduce/src/App.jsx
@@ -1,31 +1,74 @@
 import React from "react";
-import "./App.css";
-import emojipedia from "./emojipedia";
-import Card from "./components/Card";
+// import numbers from "./numbers";
 
-function createCard(emojipedia) {
-    return(
-        <Card 
-            key={emojipedia.id}
-            emoji={emojipedia.emoji}
-            name={emojipedia.name}
-            meaning={emojipedia.meaning}
-        />
-    )
-}
+// Map - Create a new array by doing something with each item in an array:
+// function App() {
+//     const newNumbers = numbers.map(function (x) {
+//         return x * 2;
+//     });
+
+//     return (
+//         console.log(newNumbers)
+//     );
+// }
+
+// Filter - Create a new array by keeping the items that return true:
+// function App() {
+//     const newNumbers = numbers.filter(function (num) {
+//         return num < 10;
+//     });
+
+//     return(
+//         console.log(newNumbers)
+//     );
+// }
+
+// Reduce - Accumulate a value by doing something to each item in an array:
+// function App() {
+//     let newNumber = numbers.reduce(function (accumulator, currentNumber) {
+//         console.log("accumulator = " + accumulator)
+//         console.log("currentNumber = " + currentNumber)
+//         return accumulator + currentNumber;
+//     });
+
+//     return(
+//         console.log(newNumber)
+//     );
+// }
+
+// Find - Find the first item that matches from an array:
+// function App() {
+//     let findNumber = numbers.find(function (num) {
+//         return num > 10;
+//     });
+
+//     return (
+//         console.log(findNumber)
+//     );
+// }
+
+// FindIndex - Find the index of the first item that matches:
+// function App() {
+//     let findIndex = numbers.findIndex(function (num) {
+//         return num > 10;
+//     })
+//     return(
+//         console.log(findIndex)
+//     );
+// }
+
+//  Challenge:
+import emojipedia from "./emojipedia";
 
 function App() {
-  return (
-    <div>
-      <h1>
-        <span>emojipedia</span>
-      </h1>
 
-      <dl className="dictionary">
-        {emojipedia.map(createCard)}
-      </dl>
-    </div>
-  );
+    let newEmojipedia = emojipedia.map(function (emojiEntry) {
+        return emojiEntry.meaning.substring(0, 100)
+    })
+
+    return(
+        console.log(newEmojipedia)
+    );
 }
 
 export default App;
---
+++ JavaScriptES6-Map-Filter-Reduce/src/numbers.js
@@ -0,0 +1,3 @@
+let numbers = [3, 56, 2, 48, 5];
+
+export default numbers;
