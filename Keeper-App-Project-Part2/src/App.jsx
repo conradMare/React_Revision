9731a0b8abcb482985d5e23acf3bc7008cf7d8f5
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-19 09:50:19 +0200

    2024/08/19(am)
    React Keeper App Project Part 2 - Project Setup

---
+++ Keeper-App-Project-Part2/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Note from "./components/Note";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
