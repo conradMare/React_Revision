5313aac7121c2ea08ff2cf6f02dca35913ba27b2
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-19 07:00:01 +0200

    2024/08/19(am)
    React Mapping Data to Components Practice - Complete

--- Mapping-Data-to-Components-Practice/src/main.jsx
+++ Mapping-Data-to-Components-Practice/src/main.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
