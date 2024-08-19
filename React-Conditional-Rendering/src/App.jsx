799fdeed728c6e87ab5a4ad50f89fc1da692b312
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-19 10:58:08 +0200

    2024/08/19(am)
    React Conditional Rendering with the Ternary Operator & AND Operator - Complete

--- React-Conditional-Rendering/src/App.jsx
+++ React-Conditional-Rendering/src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import "./App.css";
+import Login from "./components/Login";
+
+let isLoggedIn = "true";
+
+const currentTime = new Date().getHours();
 
 function App() {
-  return (
-    <div className="container">
-      <h1>Hello</h1>
-      <form className="form">
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
+    return (
+        <div className="container">
+        {/* {isLoggedIn ? <h1>Hello</h1> : <Login />} */}
+
+        {currentTime < 12 && <h1>Working</h1>}
+        </div>
+    );
 }
 
 export default App;
