882e35b14881208400a7b5f30364b2551e3ace99
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-19 10:01:03 +0200

    2024/08/19(am)
    React Keeper App Project Part 2 - Complete

--- Keeper-App-Project-Part2/src/App.jsx
+++ Keeper-App-Project-Part2/src/App.jsx
@@ -3,15 +3,24 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Note from "./components/Note";
 import "./App.css";
+import notes from "./notes";
+
+
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Note />
-      <Footer />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            {notes.map(notes =>
+                <Note
+                    key={notes.id}
+                    title={notes.title}
+                    content={notes.content}
+                />
+            )}
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
