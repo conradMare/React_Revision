34640f33cec210a1a66cf22cc92aee07898718cc
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-20 07:27:35 +0200

    2024/08/20(am)
    React Hooks - useState - Complete

--- React-Hooks-UseState/src/App.jsx
+++ React-Hooks-UseState/src/App.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 function App() {
-    return(
-        <div>
-            <h1>Hello World!</h1>
+
+    const [count, setCount] = useState(0);
+
+    // Destructuring Example:
+    // const [red, green ,blue] = [9, 132, 227];
+    // console.log(blue);
+
+    function increase() {
+        setCount(count + 1);
+    }
+
+    function decrease() {
+        setCount(count - 1);
+    }
+
+    return (
+        <div className="container">
+            <h1>{count}</h1>
+            <button onClick={increase}>+</button>
+            <button onClick={decrease}>-</button>
         </div>
     );
 }
