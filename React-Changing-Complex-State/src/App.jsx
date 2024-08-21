60fd4722f666dc63af07011065dcbcf370df96b8
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-21 09:41:54 +0200

    2024/08/21(am)
    React Changing Complex State - Project Setup

---
+++ React-Changing-Complex-State/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div className="container">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
