9d61fc5e01cf336a60b6147ad53a7fe19e4638be
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-22 06:46:26 +0200

    2024/08/22(am)
    JavaScript ES6 Spread Operator - Project Setup

---
+++ JavaScriptES6-Spread-Operator/src/App.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  const [isMouseOver, setIsMouseOver] = useState(false);
+
+  function handleChange(e) {
+    const { value, name } = e.target;
+
+    setContact(prevValue => {
+      if (name === "fName") {
+        return {
+          fName: value,
+          lName: prevValue.lName,
+          email: prevValue.email
+        }
+      } else if (name === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: value,
+          email: prevValue.email
+        }
+      } else if (name === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: value
+        }
+      }
+    })
+  }
+
+  function handleMouseOver() {
+    setIsMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setIsMouseOver(false);
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {contact.fName} {contact.lName}</h1>
+      <p>{contact.email}</p>
+      <form>
+        <input onChange={handleChange} name="fName" placeholder="First Name" />
+        <input onChange={handleChange} name="lName" placeholder="Last Name" />
+        <input onChange={handleChange} name="email" placeholder="E-Mail" />
+        <button
+          style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
