e90a4c490dfff396adb270bd89478313630502c7
conradMare <158014474+conradMare@users.noreply.github.com>
2024-08-22 06:08:57 +0200

    2024/08/22(am)
    React Changing Complex State Practice - Project Setup

--- React-Changing-Complex-State-Practice/src/App.jsx
+++ React-Changing-Complex-State-Practice/src/App.jsx
@@ -1,65 +1,26 @@
 import React, { useState } from "react";
-import "./App.css";
 
 function App() {
+    const [contact, setContact] = useState({
+        fName: "",
+        lName: "",
+        email: ""
+    });
 
-  const [isMouseOver, setIsMouseOver] = useState(false);
-
-  const [fullName, setFullName] = useState({
-    fName: "",
-    lName: ""
-  });
-
-  function handleChange(event) {
-    const { value, name } = event.target;
-
-    setFullName(prevValue => {
-      if (name === "fName") {
-        return {
-          fName: value,
-          lName: prevValue.lName
-        }
-      } else if (name === "lName") {
-        return {
-          fName: prevValue.fName,
-          lName: value
-        }
-      }
-    })
-  }
-
-  function handleMouseOver() {
-    setIsMouseOver(true);
-  }
-
-  function handleMouseOut() {
-    setIsMouseOver(false);
-  }
-
-  return (
-    <div className="container">
-      <h1>Hello {fullName.fName} {fullName.lName}</h1>
-      <form>
-        <input
-          onChange={handleChange}
-          name="fName"
-          placeholder="First Name"
-          value={fullName.fName}
-        />
-        <input
-          onChange={handleChange}
-          name="lName"
-          placeholder="Last Name"
-          value={fullName.lName}
-        />
-        <button
-          style={{ backgroundColor: isMouseOver ? "black" : "white" }}
-          onMouseOver={handleMouseOver}
-          onMouseOut={handleMouseOut}
-        >Submit</button>
-      </form>
-    </div>
-  );
+    return (
+        <div className="container">
+            <h1>
+                Hello {contact.fName} {contact.lName}
+            </h1>
+            <p>{contact.email}</p>
+            <form>
+                <input name="fName" placeholder="First Name" />
+                <input name="lName" placeholder="Last Name" />
+                <input name="email" placeholder="Email" />
+                <button>Submit</button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
